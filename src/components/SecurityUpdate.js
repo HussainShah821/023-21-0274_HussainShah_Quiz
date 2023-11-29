80d74a2a578a14b4aeb9aee924a72d9b9eb6793d
Hussainshah821 <ssyedhussain821@gmail.com>
2023-11-29 12:07:23 +0500

    Security updated

--- src/components/SecurityUpdate.js
+++ src/components/SecurityUpdate.js
@@ -3,18 +3,20 @@ import Button from "react-bootstrap/Button";
 
 const Statistics = () => {
     return (
-        <div className="col-lg-4 col-md-4 m-4">
+        <>
+        <div className="" style={{ marginLeft: '3%', marginRight: '70%'}}>
             <h1>
                 Pinpoint Security,
                 ensure the best
                 usage for the institutes
             </h1>
-            <div className="col-lg-12 col-md-8 m-3 align-items-center" >
-                We ensure the best usage for vehicles tracking and reservation system security with advance encryption standards and capability of keeping your assets like car, points much more secure with the live tracking system
             </div>
+            <div style={{marginRight:'7rem', marginLeft:'7rem', marginTop:'2rem', marginBottom:'2rem'}} ><h4>
+                We ensure the best usage for vehicles tracking and reservation system security with advance encryption standards and capability of keeping your assets like car, points much more secure with the live tracking system
+                </h4></div>
             <Button className={"align-items-center justify-content-center m-4 p-4"} variant="secondary">Get Started</Button>{' '}
 
-        </div>
+        </>
     );
 }
 
